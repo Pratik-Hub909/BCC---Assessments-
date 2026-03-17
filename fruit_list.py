0d8bd5e5ba84802e6dd8caa51020721cfed0e124
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-17 10:31:25 +0530

    Update fruit_list.py

--- fruit_list.py
+++ fruit_list.py
@@ -1,3 +1,5 @@
+
+#1
 frulist1 = ['apple', 'berry', 'cherry', 'papaya']
 frulist2 = frulist1 
 frulist3 = frulist1[:]
@@ -11,3 +13,11 @@ for ls in (frulist1, frulist2, frulist3):
     if ls[1] == 'kiwi':
         sum += 20
 print(sum)
+
+#2
+def f(i, values = []):
+    values.append(i)
+    print (values)
+f(1)
+f(2)
+f(3)    
