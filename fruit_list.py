16f102e2258b7902f61de28afb7a5fa75b6d836c
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-17 10:35:40 +0530

    Update fruit_list.py

--- fruit_list.py
+++ fruit_list.py
@@ -13,10 +13,21 @@ for ls in (frulist1, frulist2, frulist3):
         sum += 20
 print(sum)
 
+
 #2
 def f(i, values = []):
     values.append(i)
     print (values)
 f(1)
 f(2)
-f(3)    
+f(3)  
+
+
+#3
+def func(value, values):
+    var = 1
+    values[0] = 44
+t = 3
+v = [1,2,3]
+func(t,v)
+print(t, v[0])      
