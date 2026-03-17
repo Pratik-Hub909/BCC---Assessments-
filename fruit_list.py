90be4b04c76540cac4dd634f5cc44c88b365610e
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-17 10:46:57 +0530

    Update fruit_list.py

--- fruit_list.py
+++ fruit_list.py
@@ -37,3 +37,16 @@ print(t, v[0])
 dict = {'c': 97, 'a': 96, 'b': 98}
 for _ in sorted(dict):
     print(dict[_]) 
+
+
+#5
+fruit = {}
+def addone(index):
+    if index in fruit:
+        fruit[index] += 1
+    else: 
+        fruit[index] = 1
+addone('Apple')
+addone('Banana')
+addone('apple')
+print(len(fruit))
