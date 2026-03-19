5cdc9004e80c7a0fde8426692cc1c436415a0b1d
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-19 13:56:29 +0530

    Update choice.py

--- choice.py
+++ choice.py
@@ -130,6 +130,10 @@ print(s)
 val = [2**i for i in range(1, 6)] # i = 1, 2, 3, 4, 5
 print(val)
 
+#s = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+#val=[2**i for i in range(1,6)]# i= 1,2,3,4,5
+#print(val)
 
-
+val2=[i for i in s if i%2==0]#i=1
+print(val2)
 
