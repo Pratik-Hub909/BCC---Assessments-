c478e2f43ad5c0e866049848239cc02221f8345b
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-19 13:18:21 +0530

    Update choice.py

--- choice.py
+++ choice.py
@@ -39,7 +39,7 @@ Nested function
      def inner_function():
          print("This is the inner function")
      inner_function()  #Calling the inner function
- outer_function()  #Calling the outer function
+outer_function()  #Calling the outer function
 
 
 #3
