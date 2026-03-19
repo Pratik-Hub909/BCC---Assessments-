4ab81fc09dd9026449bdf7b78c63d556dbb6bcf1
Pratik-Hub909 <24tiwarip@rbunagpur.in>
2026-03-19 13:41:30 +0530

    Update choice.py

--- choice.py
+++ choice.py
@@ -104,4 +104,22 @@ print(s1)
 # Output: bbbbbbbbbbbb
 
 
+#11
+# Removing spaces from the string:
+# 1. rstrip() ===> To remove spaces at right hand side
+# 2. lstrip() ===> To remove spaces at left hand side
+# 3. strip() ===> To remove spaces both sides
+
+city = input("Enter your city Name:")
+scity = city.strip()
+
+if scity == 'Hyderabad':
+    print("Hello Hyderbadi..Adab")
+elif scity == 'Chennai':
+    print("Hello Madrasi...Vanakkam")
+elif scity == 'Bangalore':
+    print("Hello Kannadiga...Shubhodaya")
+else:
+    print("your entered city is invalid")
+
 
